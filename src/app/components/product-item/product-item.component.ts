64c5a8226d27e2b73bd00c5ec2b915021b66c206
eslammamdouh <eslam.mamdouh@parent.cloud>
2022-06-12 13:31:01 +0200

    reomove debugger

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductInterface } from 'src/app/interfaces/product.interface';
 import { ProductsService } from 'src/app/services/products.service';
@@ -8,18 +8,14 @@ import { ProductsService } from 'src/app/services/products.service';
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css']
 })
-export class ProductItemComponent implements OnInit {
-  @Input() product: ProductInterface= { id: 0, name: '', price: 0, url: '', description: ''};
+export class ProductItemComponent {
+  @Input() product: ProductInterface = { id: 0, name: '', price: 0, url: '', description: ''};
 
   constructor(private productsService: ProductsService, private router: Router) { }
 
-  ngOnInit(): void {
-  }
-
   getProductDetails(product: ProductInterface) {
-    debugger
     this.router.navigateByUrl(`product-details/${product?.id}`);
-    this.productsService.fetchProductDetails(product?.id).subscribe(res => {
+    this.productsService.fetchProductDetails(product?.id).subscribe((res: ProductInterface) => {
       console.log(res);
     })
   }
